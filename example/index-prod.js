d4d12d31f67dd0c5013ebcf26bdf0aa8478f675f
xananax <xananax@yelostudio.com>
2016-01-20 13:45:04 +0200

    corrected prod

--- example/index-prod.js
+++ example/index-prod.js
@@ -1,7 +1,7 @@
 import style from "./style.styl";
 import React from 'react';
 import ReactDOM from 'react-dom';
-var Root = require('../src/Root-Prod.js').default;
+var Root = require('../src/App.js').default;
 
 function render(){
 	ReactDOM.render(<Root/>, document.getElementById('Content'));
